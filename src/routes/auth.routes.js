4f55cd04830352a139b53d5cb5875bea682dd0eb
Beordil <Beordil@users.noreply.github.com>
2025-10-02 21:58:31 +0200

    Étape 1 : base Express + EJS (page /)

--- src/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { login, logout } = require('../controllers/auth.controller');
-
-router.post('/login', login);
-router.get('/logout', logout);
-
-module.exports = router;
